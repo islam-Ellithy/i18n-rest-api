0e44c452e3ba7a6cf4340afedfdf5f33f83a8e89
islam.emmo <islam.emmo@gmail.com>
2021-11-07 00:35:04 +0200

    updated port

--- src/main/java/com/lamaatech/testservice/services/i18nEnglishServiceImpl.java
+++ src/main/java/com/lamaatech/testservice/services/i18nEnglishServiceImpl.java
@@ -1,9 +1,12 @@
 package com.lamaatech.testservice.services;
 
 
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Service("i18nService")
+@Service("i18nServicea")
+@Profile("EN")
 public class i18nEnglishServiceImpl implements I18nService{
     @Override
     public String sayHello() {
